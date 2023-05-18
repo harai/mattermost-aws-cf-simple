255c891acd50bbf6f936df41e557f85a86bfb888
Akihiro Harai <jharai0815@gmail.com>
2023-05-18 15:46:14 +0900

    tmp

--- mattermost/main/s3.py
+++ mattermost/main/s3.py
@@ -2,7 +2,19 @@ from troposphere import StackName, Tags, s3
 
 
 def file_bucket():
-  return s3.Bucket('FileBucket')
+  return s3.Bucket(
+      'FileBucket',
+      LifecycleConfiguration=s3.LifecycleConfiguration(
+          Rules=[
+              s3.LifecycleRule(
+                  Status='Enabled',
+                  Transitions=[
+                      s3.LifecycleRuleTransition(
+                          StorageClass='STANDARD_IA', TransitionInDays=30),
+                      s3.LifecycleRuleTransition(
+                          StorageClass='GLACIER_IR', TransitionInDays=90),
+                  ]),
+          ]))
 
 
 def distribution_log_bucket():
